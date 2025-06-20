4e729d0903819e5b7d203e6a987ee75b0b475170
Angel <atapia@redrabbit.mx>
2025-06-20 09:14:43 -0600

    inicial commit

---
+++ src/main/java/atapia/demos/sisencuestas/SistemaDeEncuestasApplication.java
@@ -0,0 +1,13 @@
+package atapia.demos.sisencuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEncuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEncuestasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/atapia/demos/sisencuestas/SistemaDeEncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package atapia.demos.sisencuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
